58c395d31231bd628dd823339a144f787c0a8a1e
soundpost1682 <guarneri1682@gmail.com>
2024-12-18 12:14:59 -0800

    API-1

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
