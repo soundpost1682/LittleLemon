826b80f107172c3d154aad7ea89260d23d164b99
soundpost1682 <guarneri1682@gmail.com>
2024-12-18 13:55:17 -0800

    API-4

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
-from rest_framework.authtoken.views import obtain_auth_token
+
 
 
 urlpatterns = [
 
-    path('', views.index, name='index'),
-    path('items/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('api-token-auth/', obtain_auth_token),
+    
+    path('items/<int:pk>', SingleMenuItemView.as_view()),
+    
 
 ]
